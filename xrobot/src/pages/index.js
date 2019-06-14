91a18f6ac45dd4d8eb0747850922b7759ab7e443
Daniel Lauzon <daniel.lauzon@irdeto.com>
2019-06-14 00:48:15 -0400

    Last commit from MB00292

--- xrobot/src/pages/index.js
+++ xrobot/src/pages/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Logo from '../components/logo'
+import { XRobot } from '@daneroo/ir-logo'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={['gatsby', 'application', 'react']} />
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Logo />
+      <XRobot />
     </div>
     <Link to='/page-2/'>Go to page 2</Link>
   </Layout>
